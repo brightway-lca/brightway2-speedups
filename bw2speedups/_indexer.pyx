896de3502c58ec9ea78749a685d39e7c8a5b9861
Chris Mutel <cmutel@gmail.com>
2014-07-25 15:01:56 +0200

    0.2. Remove installation dependency on cython

--- bw2speedups/_indexer.pyx
+++ bw2speedups/_indexer.pyx
@@ -5,9 +5,15 @@ cimport cython
 DTYPE = np.uint32
 ctypedef np.uint32_t DTYPE_t
 
+
 @cython.boundscheck(False)
 def indexer(np.ndarray[DTYPE_t, ndim=1] a, np.ndarray[DTYPE_t, ndim=1] b,
-        mapping):
+            mapping):
+    """Replace each element of ``b`` with ``mapping.get(a, MAX_INT_32)``.
+
+    Modifies numpy arrays in place.
+
+    Much faster than plain python because of direct access without conversion to Numpy array."""
     cdef DTYPE_t MAX_INT_32 = 4294967295
     cdef int b_max = b.shape[0]
     for x in xrange(b_max):
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, Extension
-from Cython.Distutils import build_ext
+# from Cython.Distutils import build_ext
 import numpy as np
 
 SRC_DIR = "bw2speedups"
 
 extension_1 = Extension(SRC_DIR + "._indexer",
-                        [SRC_DIR + "/_indexer.pyx"],
+                        # [SRC_DIR + "/_indexer.pyx"],
+                        [SRC_DIR + "/_indexer.c"],
                         libraries=[],
                         include_dirs=[np.get_include()])
 
@@ -14,12 +15,12 @@ if __name__ == "__main__":
     setup(
         author="Chris Mutel",
         author_email="cmutel@gmail.com",
-        cmdclass = {'build_ext': build_ext},
+        # cmdclass = {'build_ext': build_ext},
         description="""Cython functions to speed up Brightway calculations""",
         ext_modules = [extension_1],
         install_requires=['numpy', 'cython'],
         name = 'bw2speedups',
         packages=[SRC_DIR],
-        version="0.1",
+        version="0.2",
         zip_safe=False,
     )
